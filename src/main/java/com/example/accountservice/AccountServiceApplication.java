ea6bad744c1cf1cf5158ad82e7c2af60a3ebe0a8
Ilya Chukanov <ilyachuk77@gmail.com>
2022-10-23 12:21:10 +0300

    add caching

--- src/main/java/com/example/accountservice/AccountServiceApplication.java
+++ src/main/java/com/example/accountservice/AccountServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/accountservice/service/impl/AccountServiceImpl.java
+++ src/main/java/com/example/accountservice/service/impl/AccountServiceImpl.java
@@ -3,7 +3,10 @@ package com.example.accountservice.service.impl;
 import com.example.accountservice.model.Account;
 import com.example.accountservice.repository.AccountRepository;
 import com.example.accountservice.service.AccountService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,12 +14,15 @@ import java.util.Optional;
 import static com.example.accountservice.ustils.AccountUtils.checkActive;
 
 @Service
+@Slf4j
 public class AccountServiceImpl implements AccountService {
     @Autowired
     private AccountRepository repository;
 
     @Override
-    public Long getAmount(Integer id) {
+    @Cacheable(cacheNames = "amount")
+    public synchronized Long getAmount(Integer id) {
+        log.info("find amount {}", id);
         Optional<Account> account = repository.findById(id);
         if (account.isPresent()) {
             if (checkActive(account)) {
@@ -30,12 +36,15 @@ public class AccountServiceImpl implements AccountService {
     }
 
     @Override
-    public void addAmount(Integer id, Long value) {
+    @CachePut(cacheNames = "amount", key = "#id")
+    public synchronized void addAmount(Integer id, Long amount) {
+        log.info("add amount to id = " + id + ", amount = " + amount);
         Optional<Account> account = repository.findById(id);
         if (checkActive(account)) {
-            account.ifPresent(account1 -> account1.setAmount(account1.getAmount() + value));
+            account.ifPresent(account1 -> account1.setAmount(account1.getAmount() + amount));
         } else {
             account.ifPresent(account1 -> account1.setActive(true));
+            account.ifPresent(account1 -> account1.setAmount(account1.getAmount() + amount));
         }
         repository.save(account.get());
     }
